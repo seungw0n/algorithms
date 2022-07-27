ec802df027d899b08e7256536a2a29db99145c07
Seung Won Joeng <34816905+seungw0n@users.noreply.github.com>
2022-07-27 20:45:13 +0900

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+// Baekjoon 
+
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.in));
+    
+    // code starts here
+    
+    
+    // code ends here
+    
+    bw.flush();
+    
+    bw.close();
+    br.close();
+    
+  }
+}
+
